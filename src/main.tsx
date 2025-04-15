ff72d229adbe4900ed6d30799ed87dae34398871
Jaehun Kim <wogns3623@gmail.com>
2025-04-15 17:20:56 +0900

    refactor: Add primitive routes directory

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,13 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
 
 import "./index.css";
-import App from "./App.tsx";
+import Home from "./routes/Home";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
