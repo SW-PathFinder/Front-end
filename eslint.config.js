69d72905167d08d7e2b4450c844424cb23ed50ea
Jaehun Kim <wogns3623@gmail.com>
2025-04-21 01:16:52 +0900

    tooling: Add @stylistic/eslint-plugin for formatting
    - eslint를 사용한 포매팅을 위해 라이브러리 추가 & 구성 설정

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,42 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import stylistic from "@stylistic/eslint-plugin";
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ["dist", "node_modules"] },
+  js.configs.recommended,
+  stylistic.configs.customize({
+    indent: 2,
+    quotes: "double",
+    semi: true,
+    blockSpacing: true,
+    braceStyle: "1tbs",
+    commaDangle: "only-multiline",
+    arrowParens: true,
+    jsx: true,
+  }),
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    extends: [
+      tseslint.configs.recommended,
+    ],
+    files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
+      "react-refresh/only-export-components": [
+        "warn",
         { allowConstantExport: true },
       ],
     },
   },
-)
+);
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -18,10 +18,16 @@ function Home() {
       <h1>Vite + React</h1>
       <div>
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          count is
+          {" "}
+          {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edit
+          {" "}
+          <code>src/App.tsx</code>
+          {" "}
+          and save to test HMR
         </p>
       </div>
       <p>Click on the Vite and React logos to learn more</p>
