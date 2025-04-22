6c7470e2c72ceb28ecf9f53f42d20254c811bad5
Jaehun Kim <wogns3623@gmail.com>
2025-04-22 16:01:02 +0900

    tooling: Configure eslint formatting

--- eslint.config.js
+++ eslint.config.js
@@ -6,18 +6,37 @@ import tseslint from "typescript-eslint";
 import stylistic from "@stylistic/eslint-plugin";
 import betterHooks from "@wogns3623/eslint-plugin-better-exhaustive-deps";
 
+const stylisticConfig = stylistic.configs.customize({
+  indent: 2,
+  quotes: "double",
+  semi: true,
+  blockSpacing: true,
+  braceStyle: "1tbs",
+  commaDangle: "always-multiline",
+  arrowParens: true,
+  jsx: true,
+});
+
 export default tseslint.config(
-  { ignores: ["dist", "node_modules"] },
-  stylistic.configs.customize({
-    indent: 2,
-    quotes: "double",
-    semi: true,
-    blockSpacing: true,
-    braceStyle: "1tbs",
-    commaDangle: "only-multiline",
-    arrowParens: true,
-    jsx: true,
-  }),
+  {
+    ignores: [
+      "dist",
+      "node_modules",
+    ],
+  },
+  {
+    plugins: { "@stylistic": stylistic },
+    rules: {
+      ...stylisticConfig.rules,
+      "@stylistic/array-bracket-newline": ["error", { multiline: true }],
+      "@stylistic/array-element-newline": [
+        "error",
+        { consistent: true, multiline: true, minItems: 5 },
+      ],
+      "@stylistic/object-property-newline": ["error", { allowAllPropertiesOnSameLine: true }],
+      "@stylistic/multiline-ternary": ["error", "never"],
+    },
+  },
   {
     extends: [
       js.configs.recommended,
@@ -44,9 +63,7 @@ export default tseslint.config(
         "warn",
         {
           checkMemoizedVariableIsStatic: true,
-          staticHooks: {
-            useQuery: { refetch: true },
-          },
+          staticHooks: { useQuery: { refetch: true } },
         },
       ],
     },
