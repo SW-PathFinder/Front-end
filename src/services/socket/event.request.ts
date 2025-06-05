627fd1e9d8e38121feb93728347cbfcadbc6398e
Jaehun Kim <wogns3623@gmail.com>
2025-06-05 19:10:50 +0900

    feat: Add requestId to socket emit type

--- src/services/socket/event.request.ts
+++ src/services/socket/event.request.ts
@@ -1,22 +1,30 @@
 import { SocketAction } from "@/services/socket/gameAction";
 
+type BaseEmitEvent<T> = { requestId: string } & T;
+
 export interface EmitEvents {
-  set_username: (data: { username: string }) => void;
-  create_room: (data: {
-    max_players: number;
-    is_public: boolean;
-    card_helper: boolean;
-  }) => void;
-  quick_match: (data: { max_players: number; card_helper: boolean }) => void;
-  search_room_by_code: (data: { room_code: string }) => void;
+  set_username: (data: BaseEmitEvent<{ username: string }>) => void;
+  create_room: (
+    data: BaseEmitEvent<{
+      max_players: number;
+      is_public: boolean;
+      card_helper: boolean;
+    }>,
+  ) => void;
+  quick_match: (
+    data: BaseEmitEvent<{ max_players: number; card_helper: boolean }>,
+  ) => void;
+  search_room_by_code: (data: BaseEmitEvent<{ room_code: string }>) => void;
 
-  join_game: (data: { room: string; player: string }) => void;
-  leave_room: (data: { room: string; player: string }) => void;
+  join_game: (data: BaseEmitEvent<{ room: string; player: string }>) => void;
+  leave_room: (data: BaseEmitEvent<{ room: string; player: string }>) => void;
 
-  game_action: (data: {
-    room: string;
-    player: string;
-    action: SocketAction.Request.Primitive;
-  }) => void;
-  chat: (data: { room: string; message: string }) => void;
+  game_action: (
+    data: BaseEmitEvent<{
+      room: string;
+      player: string;
+      action: SocketAction.Request.Primitive;
+    }>,
+  ) => void;
+  chat: (data: BaseEmitEvent<{ room: string; message: string }>) => void;
 }
