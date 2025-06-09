105049a0e9222da6e09cb64d29831bd9b2e26350
Kim Doo Hyun <doohyun865@gmail.com>
2025-06-09 20:31:34 +0900

    feature: waiting room UI

--- src/components/Game/PlayerList.tsx
+++ src/components/Game/PlayerList.tsx
@@ -13,7 +13,7 @@ const PlayerList = ({ list }: Props) => {
   const curRound = gameSession.round;
 
   return (
-    <div className="h-fit w-full border bg-base-100/70 shadow-xl">
+    <div className="h-fit w-full rounded border bg-base-100/70 shadow-xl">
       {list?.map((item) => <PlayerStatus key={item.id} item={item} />)}
       <div className="flex h-[54px] flex-col items-center justify-center border p-1">
         <p className="text-xl">Round {curRound} / 3</p>
